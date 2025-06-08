91257190f5c7349d839c6adad4ca5adecd40b194
alissonccosta <alissonccosta@gmail.com>
2025-06-08 10:52:27 -0300

    Adiciona pagina de solicitacao de acesso

--- verumoverview/frontend/src/App.tsx
+++ verumoverview/frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import SolicitarAcesso from './pages/SolicitarAcesso';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import Projetos from './pages/Projetos';
@@ -12,6 +13,7 @@ export default function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/solicitar-acesso" element={<SolicitarAcesso />} />
       <Route path="/" element={<Layout />}>
         <Route index element={<Dashboard />} />
         <Route path="projetos" element={<Projetos />} />
