430e94ba23d47370eac91d287e4ceafe3a934891
alissonccosta <alissonccosta@gmail.com>
2025-06-08 18:11:03 -0300

    Ajusta cores de prioridade e remove duplicidade

--- verumoverview/frontend/src/constants/design-tokens.js
+++ verumoverview/frontend/src/constants/design-tokens.js
@@ -9,10 +9,12 @@ const designTokens = {
       warning: '#FDCB6E',
       error: '#D63031'
     },
-    priority: {
-      high: '#E17055',
-      medium: '#FDCB6E',
-      low: '#0984E3'
+    prioridade: {
+      emergencial: '#D63031',
+      muitoAlta: '#E17055',
+      alta: '#FDCB6E',
+      media: '#00B894',
+      baixa: '#0984E3'
     },
     indicators: {
       positive: '#00CEC9',
